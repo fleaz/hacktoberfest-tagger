74684b5ff940964d09bb0a7d28b5b7bd531d527e
Felix Breidenstein <mail@felixbreidenstein.de>
2020-10-04 17:59:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # hacktoberfest-tagger
 Quickly add/remove the 'hacktoberfest' topic to all your public repositories
+![Screenshot of the programm output](screenshot.png)
+
+## Motivation
+Due to too many SPAM, participation in the Hacktoberfest is now opt-in. That means that you (as a maintainer) has to add
+the "hacktoberfest" topic to your repositories before the PRs will count for the other user.  Because most private
+accounts will probably not be affected this much by spam, I created this tool to quickly add/remove the topic to all
+your public repositories on GitHub.
+
+## Prerequisites
+First you need to go to the [Github developer settings](https://github.com/settings/tokens) and create an personal
+access token with the `public_repo` permission. The benefit of this in contrast to using your username&password is that
+the script only has the permissions it needs, and it also works when you have 2FA enabled(What you definitely should! ;)
+) Then you need to install the "PyGithub" library. With pipenv installed you can run `pipenv install` in the repository
+folder to create a new virtual environment with all dependencies. When you want to use pip, run `pip install --user
+pygithub`.
+
+## Usage
+```
+./run.py --help
+usage: run.py [-h] [--dry-run] {add,remove}
+
+Quickly add/remove the 'hacktoberfest' topic to all of your public Github projects
+
+positional arguments:
+  {add,remove}  'add' or 'remve' the topic
+
+optional arguments:
+  -h, --help    show this help message and exit
+  --dry-run     Don't actually modify the topics
+```
---
+++ run.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+
+from github import Github
+from github.GithubException import BadCredentialsException
+from sys import exit
+import argparse
+import os
+
+
+def write_topics(repo, topiclist, dry_run):
+    if not dry_run:
+        repo.replace_topics(topiclist)
+
+
+parser = argparse.ArgumentParser(
+    description="Quickly add/remove the 'hacktoberfest' topic to all of your public Github projects"
+)
+parser.add_argument("action", type=str, help="'add' or 'remve' the topic", choices=["add", "remove"])
+parser.add_argument("--dry-run", action="store_true", help="Don't actually modify the topics")
+args = parser.parse_args()
+
+try:
+    token = os.environ["GITHUB_TOKEN"]
+except KeyError:
+    print("You need to provide an GitHub Token via the env variable 'GITHUB_TOKEN'")
+    exit(1)
+
+g = Github(token)
+
+try:
+    user_repositories = list(g.get_user().get_repos())
+    user_id = g.get_user().id
+except BadCredentialsException:
+    print("Token is invalid or has the wrong permissions")
+    exit(1)
+
+for repo in user_repositories:
+    print(f"> Checking {repo.full_name}:")
+
+    if repo.private or repo.fork or repo.archived:
+        print("Repo is private, a fork or archived. Skipping...")
+        continue
+
+    if repo.organization is not None:
+        print("Repo belongs to a organization. Skipping...")
+        continue
+
+    if repo.owner.id != user_id:
+        print("Repo does not belong to you. Skipping...")
+        continue
+
+    topic_list = repo.get_topics()
+
+    if "hacktoberfest" in topic_list and args.action == "remove":
+        print("Removing topic from repo")
+        topic_list.remove("hacktoberfest")
+        write_topics(repo, topic_list, args.dry_run)
+    elif "hacktoberfest" not in topic_list and args.action == "add":
+        print("Adding topic to repo")
+        new_topics = topic_list + ["hacktoberfest"]
+        write_topics(repo, new_topics, args.dry_run)
+    else:
+        print("Repo in correct state")
